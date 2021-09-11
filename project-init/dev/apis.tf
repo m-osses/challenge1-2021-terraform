d994ddcbd95bea7e35f98b846410cf0cb804b680
Matias O <osses.matias@gmail.com>
2021-09-11 12:25:59 -0300

    Enable apis and add editor project permission to cloudbuild

--- project-init/dev/apis.tf
+++ project-init/dev/apis.tf
@@ -3,3 +3,23 @@ resource "google_project_service" "enable_googleapis_cloudbuild" {
   project = var.project_id
   service = "cloudbuild.googleapis.com"
 }
+
+resource "google_project_service" "enable_googleapis_compute" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "enable_googleapis_iam" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "servicenetworking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "enable_googleapis_sqladmin" {
+  project = var.project_id
+  service = "sqladmin.googleapis.com"
+}
--- project-init/dev/permissions.tf
+++ project-init/dev/permissions.tf
@@ -1,3 +1,7 @@
 ## Users & project permissions
 
-
+resource "google_project_iam_member" "cloudbuild_editor_role" {
+  project = var.project_id
+  role = "roles/editor"
+  member = "serviceAccount:${google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
