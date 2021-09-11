92a46f8dfa9922161525806ba7f4471502783cf2
Matias O <osses.matias@gmail.com>
2021-09-11 13:02:07 -0300

    Delete depends on in container registry creation

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,11 +2,3 @@
 provider "google" {
   project = "${var.project_id}"
 }
-
-data "terraform_remote_state" "project_init" {
-  backend = "gcs"
-  config = {
-    bucket  = var.tf_state_bucket_name
-    prefix  = "terraform/state/project-init"
-  }
-}
