d9600a8a70d61a9ab5e43c0ea700c6b197d89b6c
Matias O <osses.matias@gmail.com>
2021-09-11 20:41:22 -0300

    Add new database, new user and secret

--- environments/dev/sql_instance.tf
+++ environments/dev/sql_instance.tf
@@ -14,8 +14,12 @@ resource "google_service_networking_connection" "cloudsql-postgres-googleapis-co
   reserved_peering_ranges = [google_compute_global_address.private_ip_postgresql.name]
 }
 
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
 resource "google_sql_database_instance" "master" {
-  name             = "master-instance"
+  name             = "master-instance-${random_id.db_name_suffix.hex}"
   database_version = "POSTGRES_12"
   region           = var.region
 
@@ -29,4 +33,20 @@ resource "google_sql_database_instance" "master" {
       private_network = "projects/${var.project_id}/global/networks/default"
     }
   }
+}
+
+resource "google_sql_database" "challenge_db" {
+  name     = "challenge1"
+  instance = google_sql_database_instance.master.name
+}
+
+resource "random_id" "db_user_password_suffix" {
+  byte_length = 8
+}
+
+
+resource "google_sql_user" "challenge1_user" {
+  name     = "challenge1"
+  instance = google_sql_database_instance.master.name
+  password = random_id.db_user_password_suffix.hex
 }
