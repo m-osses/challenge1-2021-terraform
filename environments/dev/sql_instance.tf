22f102fc90e6a6328547632735d8fac5ca17a5c0
Matias O <osses.matias@gmail.com>
2021-09-11 18:30:19 -0300

    Add private network to DB instance

--- environments/dev/sql_instance.tf
+++ environments/dev/sql_instance.tf
@@ -1,4 +1,19 @@
 
+resource "google_compute_global_address" "private_ip_postgresql" {
+  name          = "google-managed-services-default"
+  description   = "IP Range for peer networks."
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 20
+  network       = "default"
+}
+
+resource "google_service_networking_connection" "cloudsql-postgres-googleapis-com" {
+  network                 = "default"
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_postgresql.name]
+}
+
 resource "google_sql_database_instance" "master" {
   name             = "master-instance"
   database_version = "POSTGRES_12"
@@ -8,5 +23,10 @@ resource "google_sql_database_instance" "master" {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
     tier = "db-f1-micro"
+    ip_configuration  {
+      ipv4_enabled = true
+      require_ssl = false
+      private_network = "projects/${var.project_id}/global/networks/default"
+    }
   }
 }
