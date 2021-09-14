ddefb06641c1adedb7d42bc16858b29c7efe0a4f
Matias O <osses.matias@gmail.com>
2021-09-13 22:32:34 -0300

    Fix terraform error

--- environments/dev/sql_instance.tf
+++ environments/dev/sql_instance.tf
@@ -21,38 +21,38 @@ resource "random_id" "db_name_suffix" {
   byte_length = 4
 }
 
-resource "google_sql_database_instance" "master" {
-  name             = "master-instance-${random_id.db_name_suffix.hex}"
-  database_version = "POSTGRES_12"
-  region           = var.region
-
-
-  depends_on = [google_service_networking_connection.sql_private_vpc_connection]
-
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = "db-f1-micro"
-    ip_configuration  {
-      ipv4_enabled = true
-      require_ssl = false
-      private_network = "projects/${var.project_id}/global/networks/default"
-    }
-  }
-}
-
-resource "google_sql_database" "challenge_db" {
-  name     = "challenge1"
-  instance = google_sql_database_instance.master.name
-}
-
-resource "random_id" "db_user_password_suffix" {
-  byte_length = 8
-}
-
-
-resource "google_sql_user" "challenge1_user" {
-  name     = "challenge1"
-  instance = google_sql_database_instance.master.name
-  password = random_id.db_user_password_suffix.hex
-}
+//resource "google_sql_database_instance" "master" {
+//  name             = "master-instance-${random_id.db_name_suffix.hex}"
+//  database_version = "POSTGRES_12"
+//  region           = var.region
+//
+//
+//  depends_on = [google_service_networking_connection.sql_private_vpc_connection]
+//
+//  settings {
+//    # Second-generation instance tiers are based on the machine
+//    # type. See argument reference below.
+//    tier = "db-f1-micro"
+//    ip_configuration  {
+//      ipv4_enabled = true
+//      require_ssl = false
+//      private_network = "projects/${var.project_id}/global/networks/default"
+//    }
+//  }
+//}
+//
+//resource "google_sql_database" "challenge_db" {
+//  name     = "challenge1"
+//  instance = google_sql_database_instance.master.name
+//}
+//
+//resource "random_id" "db_user_password_suffix" {
+//  byte_length = 8
+//}
+//
+//
+//resource "google_sql_user" "challenge1_user" {
+//  name     = "challenge1"
+//  instance = google_sql_database_instance.master.name
+//  password = random_id.db_user_password_suffix.hex
+//}
