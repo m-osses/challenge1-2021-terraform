6c1a12d7d170458db1ffe4e30922ce04c4c2337d
Matias O <osses.matias@gmail.com>
2021-10-28 11:46:56 -0300

    Destroy all resources

--- environments/dev/container_registry.tf
+++ environments/dev/container_registry.tf
@@ -1,6 +1,6 @@
-
-resource "google_container_registry" "registry" {
-  project = var.project_id
-  location = var.tf_state_bucket_location
-
-}
+//
+//resource "google_container_registry" "registry" {
+//  project = var.project_id
+//  location = var.tf_state_bucket_location
+//
+//}
